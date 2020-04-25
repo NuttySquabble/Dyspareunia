94d39d1815d9ab62835b6610ec711acc7daa6876
NuttySquabble <nutty.squabble@protonmail.com>
2020-04-25 11:44:05 +0300

    Created folder structure

---
+++ Source/Dyspareunia.cs
@@ -0,0 +1,23 @@
+﻿using HarmonyLib;
+using RimWorld;
+using System.Reflection;
+using Verse;
+using Verse.AI;
+
+namespace Dyspareunia
+{
+    [StaticConstructorOnStartup]
+    public static class Dyspareunia
+    {
+        static Dyspareunia()
+        {
+            Log.Message("Dyspareunia is starting.");
+            Harmony harmony = new Harmony("nuttysquabble.dyspareunia");
+            Log.Message("Harmony instance id is " + (harmony?.Id ?? "NULL"));
+            Harmony.DEBUG = true;
+            harmony.PatchAll();
+            Log.Message("Dyspareunia initialization is complete.");
+            Log.Message("Patched methods: " + harmony.GetPatchedMethods().EnumerableCount());
+        }
+    }
+}
